88f2248f7335a41527eaf01a16601ef064febe39
ayushudaydubey <102001ayushdubey@gmail.com>
2025-06-23 18:50:58 +0530

    cors

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -1,16 +1,12 @@
-
 import axios from 'axios';
 
-const isLocal = process.env.NODE_ENV === 'development';
+const isLocal = import.meta.env.MODE === 'development';
+
 
 const axiosInstance = axios.create({
   baseURL: isLocal
-
-
     ? 'http://localhost:3000'
-    : 'https://quickzy-backend.onrender.com',
-
- // render url
+    : 'https://quickzy-backend.onrender.com', // Render URL
 
   withCredentials: true,
   headers: {
