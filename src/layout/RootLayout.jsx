349bf28f5485c21f1811e4383b163b17196d4870
ayushudaydubey <102001ayushdubey@gmail.com>
2025-09-27 21:54:20 +0530

    random changes

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -7,6 +7,7 @@ const RootLayout = () => {
    <>
    <NavBar/>
    <Outlet/>
+{/* Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla voluptate nam suscipit ducimus dignissimos, ad corporis doloremque sint maxime placeat, omnis a praesentium ipsam in rerum totam optio nihil aspernatur! */}
    </>
   )
 }
